17a297dc434f910b69f72cf10a2f9f99b3498ba4
p253237 <raphael.blaauw@generali.com>
2022-03-29 10:18:05 +0200

    another init + deleting stuff

--- Frontend/npm.sh
+++
@@ -1,3 +0,0 @@
-jq 'to_entries | map_values({ (.key) : ("$" + .key) }) | reduce .[] as $item ({}; . + $item)' ./src/config.json > ./src/config.tmp.json && mv ./src/config.tmp.json ./src/config.json
-npm install
-CI=false npm run build --prod
