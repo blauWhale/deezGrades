6d5bb11d0883ea3e909b2adf9fba84516ad65e7a
zblaar <raphael.blaauw@bbcag.ch>
2022-05-03 08:28:58 +0200

    DDL name changed to deezGradesDB

--- Spring/Schema/DDL.sql
+++ Spring/Schema/DDL.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema deezGradesDB
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema deezGradesDB
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `deezGradesDB` DEFAULT CHARACTER SET utf8 ;
+USE `deezGradesDB` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Student`
+-- Table `deezGradesDB`.`Student`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Student` (
+CREATE TABLE IF NOT EXISTS `deezGradesDB`.`Student` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
@@ -25,9 +25,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Course`
+-- Table `deezGradesDB`.`Course`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Course` (
+CREATE TABLE IF NOT EXISTS `deezGradesDB`.`Course` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   `bms` TINYINT NULL,
@@ -36,9 +36,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Grades`
+-- Table `deezGradesDB`.`Grades`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Grades` (
+CREATE TABLE IF NOT EXISTS `deezGradesDB`.`Grades` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `grade` FLOAT NULL,
   `Student_id` INT NOT NULL,
@@ -48,12 +48,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Grades` (
   INDEX `fk_Grades_Course1_idx` (`Course_id` ASC) VISIBLE,
   CONSTRAINT `fk_Grades_Student`
     FOREIGN KEY (`Student_id`)
-    REFERENCES `mydb`.`Student` (`id`)
+    REFERENCES `deezGradesDB`.`Student` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Grades_Course1`
     FOREIGN KEY (`Course_id`)
-    REFERENCES `mydb`.`Course` (`id`)
+    REFERENCES `deezGradesDB`.`Course` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
